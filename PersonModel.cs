c35afe4159f3c5a1cb0e25f2ad1956d59d0340a7
Anton <anonymousStyleKiller@gmail.com>
2022-11-10 00:55:08 +0200

    LINQ: Order and OrderDescending

---
+++ PersonModel.cs
@@ -0,0 +1,16 @@
+﻿namespace LINQNetSeven;
+
+public class PersonModel : IComparable<PersonModel>
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public int CompareTo(PersonModel? other)
+    {
+        if (ReferenceEquals(this, other)) return 0;
+        if (ReferenceEquals(null, other)) return 1;
+        var nameComparison = string.Compare(Name, other.Name, StringComparison.Ordinal);
+        if (nameComparison != 0) return nameComparison;
+        return Age.CompareTo(other.Age);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LINQNetSeven;
+
+
+
+List<int> ages = new(){1,15,6,44,25};
+
+var sorted = ages.Order();
+var descending = ages.OrderDescending();
+
+foreach(var age in sorted) Console.WriteLine(age);
+Console.WriteLine(new string('-', 50));
+foreach(var i in descending) Console.WriteLine(i);
+Console.WriteLine(new string('-', 50));
+
+
+
+List<PersonModel> peoples = new()
+{
+  new(){Name = "Anton", Age = 25},
+  new (){Name = "Sasha", Age = 23},
+  new (){Name = "YURII", Age = 77}
+};
+
+
+var orderedPeople = peoples.Order();
+var desc = peoples.OrderDescending();
+
+foreach(var x in orderedPeople) Console.WriteLine($"Name {x.Name} -  Age {x.Age}");
+Console.WriteLine(new string('-', 50));
+foreach(var x in desc) Console.WriteLine($"Name {x.Name} -  Age {x.Age}");
